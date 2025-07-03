2437147e0f968267071baadb72a450a7582308db
Amit Saha <asaha.bwn2000@gmail.com>
2025-07-04 02:21:13 +0530

    progress...

---
+++ src/main/java/net/amit/journalApp/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package net.amit.journalApp.repository;
+
+import net.amit.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepo extends MongoRepository<User, ObjectId> {
+    User findByUserName(String userName);
+}
